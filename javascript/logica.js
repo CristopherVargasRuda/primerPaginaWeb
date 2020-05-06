0c3f4ccb96b812816409858086337026789140e0
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-05-05 23:42:29 -0500

    se añadio nueva linea de codigo

--- javascript/logica.js
+++ javascript/logica.js
@@ -7,7 +7,7 @@ document.getElementById("apellido1Value").innerHTML = urlParams.get('apellido1')
 //     nombre1: "Cristopher",
 //     apellido1: "apellido 1",
 // }
-
+// adsadsadsa
 
 // string url= "nombre1=Cristopher&apellido1=Vargas+Ruda&codigo1=1111111111&nombre2=nombre+2&apellido2=apellido+2&codigo2=2222222222&nombre3=nombre+3&apellido3=apellido+3&codigo3=3333333333&nombre4=nombre+4&apellido4=apellido+4&codigo4=4444444444&nombre5=nombre+5&apellido5=apellido+5&codigo5=5555555555"
 
